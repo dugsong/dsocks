afefe77126e9d7209e075fff9a2e56a0b2938c70
dugsong <dugsong@e459d587-921f-0410-8750-e906813dc064>
2008-06-04 04:36:23 +0000

    detect macosx automatically

--- trunk/dsocks-torify.sh
+++ trunk/dsocks-torify.sh
@@ -6,9 +6,11 @@ export DSOCKS_PROXY="127.0.0.1:9050"
 
 # you probably want to run tor-dns-proxy.py also...
 
-# for MacOS X...
-#LIBDSOCKS=/usr/local/lib/libdsocks.dylib
-#DYLD_INSERT_LIBRARIES=$LIBDSOCKS DYLD_FORCE_FLAT_NAMESPACE=1 exec "$@"
-
-LIBDSOCKS=/usr/local/lib/libdsocks.so.1.0
-LD_PRELOAD=$LIBDSOCKS exec "$@"
+if [ `uname -s` = "Darwin" ]; then
+  # for MacOS X...
+  LIBDSOCKS=/usr/local/lib/libdsocks.dylib
+  DYLD_INSERT_LIBRARIES=$LIBDSOCKS DYLD_FORCE_FLAT_NAMESPACE=1 exec "$@"
+else
+  LIBDSOCKS=/usr/local/lib/libdsocks.so.1.0
+  LD_PRELOAD=$LIBDSOCKS exec "$@"
+fi
